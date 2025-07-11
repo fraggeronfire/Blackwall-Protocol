b5146a49b39c420b8e3b0510a210bc82edcdecae
Dmitry Vendetta <doteriymrite@gmail.com>
2025-07-11 20:41:35 +0300

    -

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Blackwall-Protocol/', // 👈 ОБЯЗАТЕЛЬНО для GitHub Pages
   plugins: [react()],
-  base: '/cyberdream/',
 })
